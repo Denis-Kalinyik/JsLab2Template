92c7c388ec6626967e1beeb9a43212388481e259
Denis-Kalinyik <99794679+Denis-Kalinyik@users.noreply.github.com>
2023-04-11 03:35:34 +0300

    Update makePromiseWithConstructor.js

--- makePromiseWithConstructor.js
+++ makePromiseWithConstructor.js
@@ -11,3 +11,26 @@ function makePromiseWithConstructor(itShouldResolve){
 }
 
 // Add your checks here
+
+
+function makePromiseWithConstructor(itShouldResolve) {
+    return new Promise((resolve, reject) => {
+      if (itShouldResolve) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  }
+ 
+  makePromiseWithConstructor(true)
+  .then(() => console.log('Resolved'))
+  .catch(() => console.log('Rejected'));
+
+
+
+
+  makePromiseWithConstructor(false)
+    .then(() => console.log('Resolved'))
+    .catch(() => console.log('Rejected'));
+
